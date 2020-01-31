777ea68623a75045f1158236f95aedc0e62889a5
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-01-31 17:02:45 +0100

    Create csa_api.py

---
+++ csa_api/csa_api.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+import json
+import requests
+import urllib
+
+CSA_API_CREDENTIALS = {
+  "username": "thatuser",
+  "password": "thatpassword"
+}
+
+CSA_API_BASE_URL = "https://preprod.csa-ws.cineca.it/unicalPreprod"
+CSA_API_AUTH_URL = "{}{}".format(CSA_API_BASE_URL, '/authentication')
+CSA_API_PASSWORD_REFRESH = "{}{}".format(CSA_API_BASE_URL,
+                                         "/user/{username}/refreshPassword".format(**CSA_API_CREDENTIALS))
+
+CSA_API_SGE_AFFORG = "{}{}".format(CSA_API_BASE_URL, '/sge/afferenzaOrganizzativa')
+CSA_API_SGE_ESP = "{}{}".format(CSA_API_BASE_URL, '/sge/esposizioneSgePeriodo')
+
+
+class CsaConnect(object):
+    def __init__(self, base_url,
+                       username=None,
+                       password=None, token=None):
+        self.username, self.password = username, password
+
+        # if... handle token only without credentials
+        self.token = token
+
+        self.base_url = base_url
+        self.tenant = base_url.rpartition('/')[-1]
+        self.user = dict()
+
+    @staticmethod
+    def _fill_matricola(matricola):
+        return str(matricola).zfill(6)
+
+    def _get_headers(self):
+        return {'Authorization': 'bearer {}'.format(self.token)}
+
+    def auth(self):
+        req = requests.post(CSA_API_AUTH_URL,
+                            data=dict(username = self.username,
+                                      password = self.password))
+        data = json.loads(req.content.decode())
+        # TODO
+        assert 'code' not in data
+
+        self.token = data['token']
+        self.user = data['user']
+        self.tenant = self.user['tenant']
+        return self.user['verified']
+
+
+    def sge_afforg_matricola(self,
+                             matricola,
+                             dataInizio='01-01-1900',
+                             dataFine='01-01-2100'):
+        url = '{}/{}?{}'.format(CSA_API_SGE_AFFORG,
+                             self._fill_matricola(matricola),
+                             urllib.parse.urlencode(dict(dataInizio = dataInizio,
+                                                         dataFine = dataFine))
+                             )
+        req = requests.get(url,
+                           headers = self._get_headers())
+        return json.loads(req.content.decode())
+
+
+    def sge_esp(self, matricola,
+                dataRiferimento=None):
+        """elenco afferenze organizzative per risorsa umana
+           dato un periodo di riferimento
+        """
+        dataRiferimento = dataRiferimento or datetime.now().strftime('%m-%d-%Y')
+        qs = urllib.parse.urlencode(dict(matricola = self._fill_matricola(matricola),
+                                         dataRiferimento = dataRiferimento))
+        url = '{}?{}'.format(CSA_API_SGE_ESP, qs)
+        req = requests.get(url, headers = self._get_headers())
+        return json.loads(req.content.decode())
+
+
+
+if __name__ == '__main__':
+    csaconn = CsaConnect(CSA_API_BASE_URL,
+                         **CSA_API_CREDENTIALS )
